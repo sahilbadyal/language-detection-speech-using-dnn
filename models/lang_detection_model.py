2aaac8978e77cc397918c633004f23d01f0691d3
sahilbadyal <sbadyals@gmail.com>
2018-04-23 20:55:58 +0530

    Base lang detection model

---
+++ models/lang_detection_model.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+A model for language detection in speech
+"""
+import pdb
+import logging
+
+import tensorflow as tf
+from model import Model
+
+logger = logging.getLogger("langDec.model")
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+class LangDetectionModel(Model):
+    """
+    Implements special functionality for Spoken Language Detection models.
+    """
+
+    def __init__(self, helper, config, report=None):
+        self.helper = helper
+        self.config = config
+        self.report = report
+
+    def preprocess_speech_data(self, examples):
+        """Preprocess sequence data for the model.
+
+        Args:
+            examples: A list of spectrograms and corresponding language input/output sequences.
+        Returns:
+            A new list of vectorized input/output pairs appropriate for the model.
+        """
+        raise NotImplementedError("Each Model must re-implement this method.")
+
+    def evaluate(self, sess, examples, examples_raw):
+        """Evaluates model performance on @examples.
+
+        This function uses the model to predict labels for @examples and constructs a confusion matrix.
+
+        Args:
+            sess: the current TensorFlow session.
+            examples: A list of vectorized input/output pairs.
+            examples: A list of the original input/output sequence pairs.
+        Returns:
+            The F1 score for predicting input as language entities.
+        """
+        #IMPLEMENTATION OF EVALUATION FUNCTION HERE
+        p = correct_preds / total_preds if correct_preds > 0 else 0
+        r = correct_preds / total_correct if correct_preds > 0 else 0
+        f1 = 2 * p * r / (p + r) if correct_preds > 0 else 0
+        return  (p, r, f1)
+
+
+    def output(self, sess, inputs_raw, inputs=None):
+        """
+        Reports the output of the model on examples (uses helper to featurize each example).
+        """
+        if inputs is None:
+            inputs = self.preprocess_sequence_data(self.helper.vectorize(inputs_raw))
+
+        preds = []
+        prog = Progbar(target=1 + int(len(inputs) / self.config.batch_size))
+        for i, batch in enumerate(minibatches(inputs, self.config.batch_size, shuffle=False)):
+            # Ignore predict
+            batch = batch[:1] + batch[2:]
+            preds_ = self.predict_on_batch(sess, *batch)
+            preds += list(preds_)
+            prog.update(i + 1, [])
+        return self.consolidate_predictions(inputs_raw, inputs, preds)
+
+    def fit(self, sess, saver, train_examples_raw, dev_set_raw):
+        best_score = 0.
+
+        train_examples = self.preprocess_speech_data(train_examples_raw)
+        dev_set = self.preprocess_speech_data(dev_set_raw)
+
+        for epoch in range(self.config.n_epochs):
+            logger.info("Epoch %d out of %d", epoch + 1, self.config.n_epochs)
+            # You may use the progress bar to monitor the training progress
+            # Addition of progress bar will not be graded, but may help when debugging
+            n_minibatches = 1 + int(len(train_examples) / self.config.batch_size)
+            prog = Progbar(target=n_minibatches)
+			# The general idea is to loop over minibatches from train_examples, and run train_on_batch inside the loop
+			# Hint: train_examples could be a list containing the feature data and label data
+			# Read the doc for utils.get_minibatches to find out how to use it.
+                        # Note that get_minibatches could either return a list, or a list of list
+                        # [features, labels]. This makes expanding tuples into arguments (* operator) handy
+
+            ### YOUR CODE HERE (2-3 lines)
+            for i,batch in enumerate(minibatches(train_examples,self.config.batch_size)):
+                    loss = self.train_on_batch(sess,*batch)
+                    prog.update(i+1,[("loss",loss)])
+            ### END YOUR CODE
+
+            logger.info("Evaluating on development data")
+            language_cm, entity_scores = self.evaluate(sess, dev_set, dev_set_raw)
+            logger.debug("Lang-level confusion matrix:\n" + language_cm.as_table())
+            logger.debug("Lang-level scores:\n" + token_cm.summary())
+            logger.info("Entity level P/R/F1: %.2f/%.2f/%.2f", *entity_scores)
+
+            score = entity_scores[-1]
+            
+            if score > best_score:
+                best_score = score
+                if saver:
+                    logger.info("New best score! Saving model in %s", self.config.model_output)
+                    saver.save(sess, self.config.model_output)
+            print("")
+            if self.report:
+                self.report.log_epoch()
+                self.report.save()
+        return best_score
